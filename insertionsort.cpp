7ca2b1083025e791c11ceefc05fd9375ec87e3e5
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2025-03-24 20:55:02 +0700

    insertion_4_1

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -3,3 +3,25 @@
 
  int arr[20];
  int n;
+
+ void input()
+ {
+    while (true)
+    {
+        cout << "Masukkan Jumlah data pada array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n";
+        }
+    }
+    cout << endl;
+    cout << "======================" << endl;
+    cout << "Masukkan Element Array" << endl;
+    cout << "======================" << endl;
+ }
